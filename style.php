cdc135b0a696d79758c391cd1b40228b64d3fca8
fabiocaettano <fabio.caettano74@gmail.com>
2021-07-20 22:35:54 -0300

    teste css

--- style.php
+++ style.php
@@ -1,25 +1,15 @@
 <?php header("Content-type: text/css"); ?>
 
-@media only screen and (max-device-width: 900px) {
-  /* .menu { width:600px; } */
-  .div1 {
-    width: 500px;
-    height: 100px;
-    border: 1px solid blue;
-    box-sizing: border-box;
+body {
+  background-color: gray;
+}
+
+@media screen and (min-width: 480px) {
+  body {
+    background-color: lightgreen;
   }
 }
 
-@media only screen and (max-device-width: 700px) {
-  /* .menu { width:400px; } */
-  .div1 {
-    width: 200px;
-    height: 100px;  
-    padding: 50px;
-    border: 1px solid red;
-    box-sizing: border-box;
-  }
-}
 
 
 
